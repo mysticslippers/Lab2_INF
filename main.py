903c707788456811df299645fbd57ed0fd465633
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-09-30 20:58:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,49 +5,47 @@
 #Задание №9
 
 
-n = input()   #Вводим число в двоичной системе счисления
-
-r = [int(i) for i in n[4::]] #Делим строку на информационное сообщение и биты чётности
-k = [int(i) for i in n[:4]]
-
-si = [r[0] ^ k[0] ^ k[1] ^ k[3], r[1] ^ k[0] ^ k[2] ^ k[3], r[2] ^ k[1] ^ k[2] ^ k[3]]
-s = ''.join([str(i) for i in si])
-
-ind = 0
-if s == '000':
-    print(n)
-    print("Нет ошибок.")
-else:
-    s = s[::-1]
-    for i in range(len(s)):
-        ind += int(s[i]) * 2 ** i
-    ind = str(ind)
-    print("Ошибка на позиции: " + ind)
-    if ind in '124':
-        if r[int(ind) // 2] % 2 == 0:
-            r[int(ind) // 2] = 1
-        else:
-            r[int(ind) // 2] = 0
-            print(''.join([str(i) for i in k]) + ''.join([str(i) for i in r]))
+n = input()
+i, r = [], []
+for k in range(len(n)):
+    if str(k) in '013':
+        r.append(int(n[k]))
     else:
-        if ind == '3':
-            if k[0] % 2 == 0:
-                k[0] = 1
-            else:
-                k[0] = 0
-        elif ind == '5':
-            if k[1] % 2 == 0:
-                k[1] = 1
-            else:
-                k[1] = 0
-        elif ind == '6':
-            if k[2] % 2 == 0:
-                k[2] = 1
-            else:
-                k[2] = 0
+        i.append(int(n[k]))
+si = [r[0] ^ i[0] ^ i[1] ^ i[3], r[1] ^ i[0] ^ i[2] ^ i[3], r[2] ^ i[1] ^ i[2] ^ i[3]]
+si.reverse()
+s = ''.join([str(k) for k in si])
+if s != '000':
+    ind = 0
+    s = s[::-1]
+    for k in range(len(s)):
+        ind += int(s[k]) * 2 ** k
+    print('Ошибка в позиции: ' + str(ind))
+    if str(ind) in '124':
+        if r[ind // 2] % 2 == 0:
+            r[ind // 2] = 1
         else:
-            if k[3] % 2 == 0:
-                k[3] = 1
-            else:
-                k[3] = 0
-        print(''.join([str(i) for i in k]) + ''.join([str(i) for i in r]))
+            r[ind // 2] = 0
+    elif ind == 3:
+        if i[0] % 2 == 0:
+            i[0] = 1
+        else:
+            i[0] = 0
+    elif ind == 5:
+        if i[1] % 2 == 0:
+            i[1] = 1
+        else:
+            i[1] = 0
+    elif ind == 6:
+        if i[2] % 2 == 0:
+            i[2] = 1
+        else:
+            i[2] = 0
+    else:
+        if i[3] % 2 == 0:
+            i[3] = 1
+        else:
+            i[3] = 0
+    print(''.join([str(k) for k in r[:2]]) + str(i[0]) + str(r[2]) + ''.join([str(k) for k in i[1:]]))
+else:
+    print('Ошибки нет!')
